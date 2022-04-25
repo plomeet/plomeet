d9677bad7c8692112def7c56be8a9efbaa8e1d0f
박찬혁 <acardiav@gmail.com>
2022-04-25 20:57:08 +0900

    � chore : 초기 세팅 업로드

---
+++ backend/src/test/java/com/ssafy/PloMeet/PloMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.PloMeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
