ed871796eed71bb9fbf6cd8e4bded517fff65a15
박찬혁 <acardiav@gmail.com>
2022-05-16 23:22:47 +0900

    :rocket: feat : 프로필 수정 컴포넌트 조건부 렌더링

--- frontend/app/components/my/profile/profile.js
+++ frontend/app/components/my/profile/profile.js
@@ -1,5 +1,5 @@
-import React, { Component, Node, Button } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React, { Component, Node, Button, useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity } from "react-native";
 import { useSelector } from 'react-redux';
 import { ProfileContainer, ProfileImage } from "./styles";
 
@@ -7,18 +7,60 @@ const Profile = () => {
     const nickname = useSelector(state => state.nickname)
     const img = useSelector(state => state.img)
     const email = useSelector(state => state.email)
+
+    const [index, setIndex] = useState(true);
+    const [index2, setIndex2] = useState(true);
+    const [value, onChangeText] = useState("");
+
+    const updateNickname = () => {
+        setIndex(false);
+        setIndex2(false);
+    }
+
+    const confirmNickname = () => {
+        setIndex(true);
+        setIndex2(true);
+    }
+
+    var input = null;
+    if (index2) {
+        input = <Text style = {styles.Text}>{nickname}</Text>
+    } else {
+        input = <TextInput style={styles.input}
+        placeholder="닉네임"
+        keyboardType="default"
+        autoFocus
+        maxLength={10}
+        autoCapitalize="none"
+        onChangeText={text => onChangeText(text)}
+        value={value}></TextInput>
+    }
+
+    var button = null;
+    if (index) {
+        button = <TouchableOpacity style= {styles.TouchableOpacityStyle} onPress={updateNickname}>
+                    <Text style = {styles.Text3}>수정</Text>
+                </TouchableOpacity>;
+    } else {
+        button = <TouchableOpacity style= {styles.TouchableOpacityStyle} onPress={confirmNickname}>
+                    <Text style = {styles.Text3}>완료</Text>
+                </TouchableOpacity>;
+    }
+
     return(
         <View style = {styles.Container}>
             <TouchableOpacity >
                 <Image style = {styles.ProfileImage} source={{uri: img}}></Image>
             </TouchableOpacity>
             <View>
-                <Text style = {styles.Text}>{nickname}</Text>
+                {input}
+                {/* <Text style = {styles.Text}>{nickname}</Text> */}
                 <Text style = {styles.Text2}>{email}</Text>
             </View>
-            <TouchableOpacity style= {styles.TouchableOpacityStyle}>
+            {button}
+            {/* <TouchableOpacity style= {styles.TouchableOpacityStyle} onPress={updateNickname}>
                 <Text style = {styles.Text3}>수정</Text>
-            </TouchableOpacity>
+            </TouchableOpacity> */}
         </View>
     )
 };
@@ -51,6 +93,13 @@ const styles = StyleSheet.create({
         color:'#000000'
         // backgroundColor: '#808080'
     },
+    input: {
+        width: 170,
+        height: 42,
+        fontSize: 20,
+        left: 40,
+        top: 22,
+    },
     Text2: {
         width: 170,
         height: 25,
@@ -74,6 +123,7 @@ const styles = StyleSheet.create({
     },
     Text3: {
         fontSize: 16,
+        color: '#FFFFFF'
     }
 })
 
