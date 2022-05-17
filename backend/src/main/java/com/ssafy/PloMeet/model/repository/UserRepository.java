fe64f91599327c18696a1ccc5e131ad07454b896
yunsoo <yyss241@gmail.com>
2022-05-17 12:08:25 +0900

    🚀 feat : [BE] only for Chat Test

--- backend/src/main/java/com/ssafy/PloMeet/model/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/PloMeet/model/repository/UserRepository.java
@@ -9,4 +9,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long id);
     Optional<User> findByKakaoUserId(Long kakaoUserId);
 
+    User findByUserId(Long userId);
 }
