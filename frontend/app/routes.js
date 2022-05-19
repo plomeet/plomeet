5109426733b16b3615fcccbb2f9df4555de898ff
이지우 <ckdn9595@naver.com>
2022-05-19 15:59:32 +0900

    🔥 hotfix : fix route plogging button

--- frontend/app/routes.js
+++ frontend/app/routes.js
@@ -30,8 +30,8 @@ import PolicyDoc from './components/my/preference/policyDoc/index'
 
 //Chatting
 import InsideRoom from './components/chat/room/inside_room';
-import {SideBar_} from './components/chat/room/DrawerNavigator';
-import {DrawerNavigator} from './components/chat/room/DrawerNavigator'
+import { SideBar_ } from './components/chat/room/DrawerNavigator';
+import { DrawerNavigator } from './components/chat/room/DrawerNavigator'
 
 //My
 import BadgeList from './components/my/badge/badge_all';
@@ -63,7 +63,6 @@ const AppTabComponent = () => {
                     }
                     if (route.name === '플로깅') return (
                         <TouchableOpacity
-                            hitSlop={{ top: 5.5, bottom: 20, left: 10, right: 10 }}
                             onPress={() => navigation.navigate('ploggingActivity', { msg: "plogging start" })}>
                             <View style={style.backGround}>
                                 <Image
@@ -84,7 +83,7 @@ const AppTabComponent = () => {
         >
             <MainScreenTab.Screen name="홈" component={Home} />
             <MainScreenTab.Screen name="기록" component={RecordContainer} />
-            <MainScreenTab.Screen name="플로깅" component={Plogging} options={{ headerShown: false }} />
+            <MainScreenTab.Screen name="플로깅" component={Plogging} options={{ headerShown: false, tabBarShowLabel: false, tabBarStyle: { height: 0 } }} />
             <MainScreenTab.Screen name="채팅 목록" component={Chat} />
             <MainScreenTab.Screen name="MY" component={MyPage} options={{ title: 'MY', headerShown: false }} />
         </MainScreenTab.Navigator>
@@ -113,7 +112,7 @@ export const RootNavigator = () => {
             <Stack.Screen name="M" component={AppTabComponent} options={{ title: ' ', headerShown: false }} />
             <Stack.Screen name="ploggingActivity" component={Plogging} options={{ title: '', headerShown: false, gestureEnabled: false }} />
             <Stack.Screen name="CameraPage" component={CameraPage} options={{ headerShown: false }} />
-            <Stack.Screen name="InChatRoom" component={SideBar_}/>
+            <Stack.Screen name="InChatRoom" component={SideBar_} />
             <Stack.Screen name="OpenMeeting" component={OpenMeeting} options={{ title: '플로깅 모임 생성(1/5)' }} />
             <Stack.Screen name="OpenMeeting2" component={OpenMeeting2} options={{ animationEnabled: false, title: '플로깅 모임 생성(2/5)' }} />
             <Stack.Screen name="OpenMeeting3" component={OpenMeeting3} options={{ animationEnabled: false, title: '플로깅 모임 생성(3/5)' }} />
